c1ed51889276ccd3e6ea4e8e7178941a02b5928e
Mathew Han <mathewjhan@gmail.com>
2020-12-18 18:35:53 -0800

    basic homepage

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
+import { Route, BrowserRouter as Router, Switch, Redirect } from "react-router-dom";
+import HomePage from './components/HomePage';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Router>
+          <Switch>
+            <Route
+              exact
+              path="/notes"
+            />
+            <Route
+              exact
+              path="/"
+              component={HomePage}
+            />
+            <Route>
+              <Redirect to="/" />
+            </Route>
+          </Switch>
+        </Router>
       </div>
     );
   }
---
+++ src/components/HomePage.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const HomePage = props => {
+  return (
+    <div className="container">
+      <div className="login-page h-100 row">
+        <div className="col" />
+        <div className="col d-flex align-items-center">
+          <div className="card login-container">
+            <div id="welcome">
+              Welcome to our HackUMass project LUL!
+            </div>
+          </div>
+        </div>
+        <div className="col" />
+      </div>
+    </div>
+  )
+}
+
+export default HomePage;
